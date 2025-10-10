8848bf1f850860d6cc3e1a0ce8e1f3035ee2429a
Zesk <nhatkhang25092005@gmail.com>
2025-10-10 10:41:05 +0700

    set up web project structure (frontend and backend)

---
+++ page/frontend/run.py
@@ -0,0 +1,19 @@
+from app import create_app
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+# ( test basic flask app )
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
